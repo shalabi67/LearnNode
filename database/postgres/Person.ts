678c49d3283416177800ba141acd00fe01dce767
shalabi67 <OmarAsyle+123>
2019-10-22 12:31:22 +0200

    adding postgres async support

---
+++ database/postgres/Person.ts
@@ -0,0 +1,6 @@
+export class Person {
+    id: number = 0;
+    first_name: string = '';
+    last_name: string = '';
+    email: string = '';
+}
--- database/postgres/example.ts
+++ database/postgres/example.ts
@@ -1,4 +1,5 @@
 import {Client, ClientConfig} from 'pg';
+import {Person} from "./Person";
 
 
 let configuration: ClientConfig = {
@@ -10,8 +11,9 @@ let configuration: ClientConfig = {
 }
 const client = new Client(configuration);
 const query = 'SELECT id, first_name, last_name, email FROM person';
-async function main(client : Client, query: string, values?:Array<any>) {
-    console.log("main");
+async function getData(client : Client, query: string, values?:Array<any>):Promise<Array<Person>> {
+    console.log("getData");
+    let data: Array<Person> = new Array<Person>();
     try {
         await client.connect();
         console.log('connected.');
@@ -20,8 +22,9 @@ async function main(client : Client, query: string, values?:Array<any>) {
         console.log(queryResult.rows);
         console.table(queryResult.rows); //will not display anything, but it will be used in debugger
 
-
         for (const row of queryResult.rows) {
+            let person: Person = row;
+            data.push(person);
             console.log(row.first_name);
         }
     } catch (e) {
@@ -29,7 +32,9 @@ async function main(client : Client, query: string, values?:Array<any>) {
     }
 
     await client.end();
+
+    return data;
 }
 
-main(client, query);
-main(new Client(configuration), query+' where first_name=$1', ["mohammad"]);
+getData(client, query);
+getData(new Client(configuration), query+' where first_name=$1', ["mohammad"]);
